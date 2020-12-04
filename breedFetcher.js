d2eccd70f156732e936554e0fd4d82641dadd6ad
Michael Xiang <yorkmike4@gmail.com>
2020-12-04 02:18:56 +0000

    cats

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,24 @@
 const request = require("request");
-const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?name=${breed}`, (err, response, body) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  if (body.length === 2) {
-    console.log("breed not found.");
-  } else {
-    const data = JSON.parse(body);
-    console.log(data);
-  }
-});
+// const errDetec = (err,body) => {
+//   if(err = null) {
+//     return body;
+//   } else {
+//     return err;
+//   }
+// };
+
+const fetchBreedDescription = function (breed, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?name=${breed}`, (err, response, body) => {
+    let desc;
+    if (body.length === 2) {
+      desc = null;
+      err = "breed not found";
+    } else {
+      desc = JSON.parse(body)[0].description;
+    }
+    callback(err, desc);
+  });
+};
+
+module.exports = { fetchBreedDescription };
